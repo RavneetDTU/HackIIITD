83848a1507ba3ec837defb743ee2136b68c6535d
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-08-25 23:47:41 +0530

    Login Created

---
+++ app/src/main/java/com/mafitness/hackiiitd/Infracture/data.kt
@@ -0,0 +1,12 @@
+package com.mafitness.hackiiitd.Infracture
+
+/**
+ * Created by ravneet on 25/8/17.
+ */
+data class data(
+        val owner : String,
+        val industryname : String,
+        val phone : String,
+        val udyog : String,
+        val cluster : String
+)
---
+++ app/src/main/java/com/mafitness/hackiiitd/MainActivity.java
@@ -0,0 +1,62 @@
+package com.mafitness.hackiiitd;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    public FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    public static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+
+        }else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setProviders(
+                                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign-In Canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
+
