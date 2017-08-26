07c84822abe4aae70dfe9e2f26aa731b2e54c525
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-08-26 10:04:52 +0530

    adapter done

--- app/src/main/java/com/mafitness/hackiiitd/adapter/adapter2.java
+++ app/src/main/java/com/mafitness/hackiiitd/adapter/adapter2.java
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mafitness.hackiiitd.Infracture.data;
+import com.mafitness.hackiiitd.Infracture.marketdata;
 import com.mafitness.hackiiitd.R;
 
 import java.util.ArrayList;
@@ -18,19 +20,19 @@ import java.util.ArrayList;
 public class adapter2 extends RecyclerView.Adapter<adapter2.adapter2ViewHolder> {
 
     private Context context;
-    private ArrayList<data> dataList;
+    private ArrayList<marketdata> dataList;
     private OnItemClickListner onItemClickListner;
 
     public void setOnClickListener(OnItemClickListner onItemClickListner){
         this.onItemClickListner = onItemClickListner;
     }
 
-    public adapter2(Context context , ArrayList<data> dataList){
+    public adapter2(Context context , ArrayList<marketdata> dataList){
         this.dataList = dataList;
         this.context = context;
     }
 
-    public void updatelist(ArrayList<data> dataList){
+    public void updatelist(ArrayList<marketdata> dataList){
         this.dataList = dataList;
         notifyDataSetChanged();
 
@@ -39,14 +41,19 @@ public class adapter2 extends RecyclerView.Adapter<adapter2.adapter2ViewHolder> 
     @Override
     public adapter2ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View thisview = li.inflate(R.layout.listiem,parent,false);
+        View thisview = li.inflate(R.layout.marketplace_list_item,parent,false);
 
         return new adapter2.adapter2ViewHolder(thisview);
     }
 
     @Override
     public void onBindViewHolder(adapter2ViewHolder holder, int position) {
+        marketdata thisdata = dataList.get(position);
 
+        holder.tvDetails.setText(thisdata.getDetails());
+        holder.tvCity.setText(thisdata.getCity());
+        holder.tvPhoneNum.setText(thisdata.getPhonenum());
+        holder.tvIndName.setText(thisdata.getIndustryname());
     }
 
     @Override
@@ -56,8 +63,14 @@ public class adapter2 extends RecyclerView.Adapter<adapter2.adapter2ViewHolder> 
 
     class adapter2ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView tvIndName, tvPhoneNum, tvCity, tvDetails;
+
         public adapter2ViewHolder(View itemView) {
             super(itemView);
+            tvCity = itemView.findViewById(R.id.tv_MA2_city);
+            tvIndName = itemView.findViewById(R.id.tv_MA2_indName);
+            tvPhoneNum = itemView.findViewById(R.id.tv_MA2_phonenum);
+            tvDetails = itemView.findViewById(R.id.tv_MA2_details);
         }
     }
 }
