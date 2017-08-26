d522b93b3857cb85d4fbc8290b225a7f90985265
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-08-26 09:29:43 +0530

    Fab Button Problem

--- app/src/main/java/com/mafitness/hackiiitd/MainActivity.java
+++ app/src/main/java/com/mafitness/hackiiitd/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,14 +19,48 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.mafitness.hackiiitd.Infracture.data;
+import com.mafitness.hackiiitd.adapter.adapter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    public FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+    private adapter adapter1;
     private FirebaseAuth.AuthStateListener authStateListener;
     public static final int RC_SIGN_IN = 1;
+    RecyclerView recyclerView;
+    private ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            ArrayList<data> dataArrayList = new ArrayList<>();
+            for(DataSnapshot data : dataSnapshot.getChildren()){
+                data thisdata = new data(data.child("owner").getValue().toString(),
+                        data.child("industryname").getValue().toString(),
+                        data.child("phone").getValue().toString(),
+                        data.child("udyog").getValue().toString(),
+                        data.child("decription").getValue().toString(),
+                        data.child("cluster").getValue().toString(),
+                        data.child("logourl").getValue().toString(),
+                        data.child("email").getValue().toString(),
+                        data.child("location").getValue().toString());
+                dataArrayList.add(thisdata);
+            }
+            adapter1.updatelist(dataArrayList);
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -51,6 +87,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(new Intent(MainActivity.this,UploadActivity.class));
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter1 = new adapter(this,new ArrayList<data>());
+        recyclerView.setAdapter(adapter1);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
